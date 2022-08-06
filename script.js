3167736498d5985cce583ebbb4b279e9cc580cff
Ibrahim San <ibrahimattiasan@gmail.com>
2022-08-06 09:46:55 +0400

    Navbar 60% completed for big screen
    Challenge is 20% completed

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-document.addEventListener("click", (e) => { 
+document.addEventListener("click", e => { 
+    const isDropDownButton = e.target.matches("[data-dropdown-button]")
+    if (!isDropDownButton && e.target.closest("[data-dropdown]") != null) return
 
-    const isDropDownButton = e.target.matches("button[data-dropdown-button]") ; 
-
-    if(!isDropDownButton && e.target.closest("div[data-dropdown]") != null) return; 
-
-
-    let currentDropDown; 
-    if(isDropDownButton) { 
-        currentDropDown = e.target.closest('[data-dropdown]'); 
-        currentDropDown.classlist.toggle("active"); 
+    let currentDropDown
+    if (isDropDownButton) { 
+        currentDropDown = e.target.closest("[data-dropdown]")
+        currentDropDown.classList.toggle("active")
     }
 
     document.querySelectorAll("[data-dropdown].active").forEach(dropdown => { 
-        if(dropdown === currentDropDown) return;
-        dropdown.classList.remove('active');    
+        if (dropdown === currentDropDown) return
+        dropdown.classList.remove('active')    
     })
 
 })
