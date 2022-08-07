eea1432331c27c77bc6b024ea7b5b4ca43ae9f93
imsan <ibrahimattiasan@gmail.com>
2022-08-07 11:20:28 -0800

    50% of the challenge is completed

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 document.addEventListener("click", e => { 
     const isDropDownButton = e.target.matches("[data-dropdown-button]")
-    if (!isDropDownButton && e.target.closest("[data-dropdown]") != null) return
+    const isArrowDown = e.target.matches("[data-arrow-down]")
+    const isArrowUp = e.target.matches("[data-arrow-up]")
+    if ((!isDropDownButton && !isArrowDown && !isArrowUp) && e.target.closest("[data-dropdown]") != null) return
+
+    
 
     let currentDropDown
-    if (isDropDownButton) { 
+    if (isDropDownButton || isArrowDown) { 
         currentDropDown = e.target.closest("[data-dropdown]")
         currentDropDown.classList.toggle("active")
-    }
+    } 
+ 
 
+    
     document.querySelectorAll("[data-dropdown].active").forEach(dropdown => { 
         if (dropdown === currentDropDown) return
         dropdown.classList.remove('active')    
     })
 
+    
+    
 })
